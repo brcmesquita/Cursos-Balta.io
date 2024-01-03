53b5c5b9823031d0a68729bb081a93fa8dd3fbfb
Bruno Raphael Cabral de Mesquita <brcmesquita@gmail.com>
2024-01-03 12:38:17 -0300

    update folders and files

---
+++ fundamentos_c-sharp/MeuApp/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int idade;
+idade = 39;
+
+System.Console.WriteLine(idade);
+
+const int ANO_NASCIMENTO = 1984;
+
+System.Console.WriteLine(ANO_NASCIMENTO);
+
+int soma = idade + ANO_NASCIMENTO;
+
+System.Console.WriteLine(soma);
+
